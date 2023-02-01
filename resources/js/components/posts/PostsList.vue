1449b8cbdec5851c0d2128901307c543e50810c6
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2023-02-01 15:00:14 +0100

    qualcosa

---
+++ resources/js/components/posts/PostsList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="elem in posts" :key="elem.id"> {{ elem.title }} </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'PostsList',
+    props: {
+        posts: Array
+    }
+}
+
+</script>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -7,12 +7,23 @@
                 >
             </li>
         </ul>
+        <div>
+        <ul>
+            <li v-for="elem in posts" :key="elem.id"> {{ elem.title }} </li>
+        </ul>
     </div>
+    </div>
+
+
+
 </template>
 
 <script>
 export default {
     name: "App",
+    props: {
+        posts: Array
+    },
     components: {
         //qui inseriremo i componenti
     },
