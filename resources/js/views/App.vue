c903e1d2d1f20f7bf625052eeb8735ce2a204341
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2023-02-01 14:51:21 +0100

    app.vue

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -16,5 +16,28 @@ export default {
     components: {
         //qui inseriremo i componenti
     },
+    data() {
+        return {
+            posts: []
+        }
+    },
+    mounted() {
+        this.getPosts();
+    },
+    beforeUpdate() {
+
+    },
+    methods: {
+        getPosts(){
+            axios.get('http://127.0.0.1:8000/api/posts').then((res)=>{
+                console.log(res.data);
+                this.posts = res.data
+            })
+        }
+    },
 };
+
+
+
+
 </script>
